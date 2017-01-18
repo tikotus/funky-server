bfd2559a928e12247c0a0890495781c621575bf5
Tikotus <jahvenni@gmail.com>
2017-01-19 01:26:48 +0200

    Replaces socket server with websocket for now. Works somewhat.

--- src/funky_server/aleph_core.clj
+++ src/funky_server/aleph_core.clj
@@ -12,10 +12,19 @@
 (def system-newline ;; This is in clojure.core but marked private.
   (System/getProperty "line.separator"))
 
+(def non-websocket-request
+  {:status 400
+   :headers {"content-type" "application/text"}
+   :body "Expected a websocket request."})
+
+(defn- handle-message [msg]
+  (log/info (String. msg))
+  (json/read-str (String. msg) :key-fn keyword))
+
 (defn- init-socket [stream]
   (log/info "Initing new async socket")
-  (let [in-ch (async/chan (async/sliding-buffer 1) (map #(json/read-str (String. %) :key-fn keyword)) #(log/error "Error in received message" %))
-        out-ch (async/chan 8 (map #(str (json/write-str %) system-newline)) #(log/error "Error in sent message" %))
+  (let [in-ch (async/chan (async/sliding-buffer 1) (map handle-message) #(log/error "Error in received message" %))
+        out-ch (async/chan 8 (map #(.getBytes (str (json/write-str %) system-newline))) #(log/error "Error in sent message" %))
         public-socket {:in in-ch :out out-ch}]
     
     (s/connect stream in-ch)
@@ -24,6 +33,15 @@
     (log/info "New async socket opened")
     public-socket))
 
+(defn init-websocket [req]
+  (log/info "Init websocket")
+  (if-let [socket (try
+                    @(http/websocket-connection req {:headers {:Sec-WebSocket-Protocol "binary"}})
+                    (catch Exception e nil))]
+    socket
+    (do 
+        (log/info "Not websocket")
+        non-websocket-request)))
 
 (defn socket-server [port]
   (let [connections (async/chan 50 (map init-socket))
@@ -31,6 +49,11 @@
     (log/info "Starting async server at port" port)
     { :port port :connections connections :server aleph-server}))
 
+(defn websocket-server [port]
+  (let [connections (async/chan 50 (map init-socket))
+        aleph-server (http/start-server #(let [s (init-websocket %)] (log/info "Got connection") (async/>!! connections s) s) {:port port})]
+    (log/info "Starting async server at port" port)
+    { :port port :connections connections :server aleph-server}))
 
 (defn start-game [max-players id step-time]
   (log/info "Starting game with id" id ", max-players" max-players ", step-time" step-time)
@@ -87,7 +110,7 @@
 
 (defn -main []
   (tcp/start-server echo-handler {:port 10001})
-  (async/<!! (start-lockstep-server (socket-server 8888))))
+  (async/<!! (start-lockstep-server (websocket-server 8889))))
 
 
 ;;(.close (:server server))
