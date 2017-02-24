41a37654e5f7e89a3590450bdc7d963f52587cf3
Tikotus <jahvenni@gmail.com>
2017-02-24 11:44:46 +0200

    Adds filler messages to fight negle's

--- src/funky_server/aleph_core.clj
+++ src/funky_server/aleph_core.clj
@@ -30,7 +30,8 @@
     (s/connect out-ch stream)
 
     (future (loop []
-      (Thread/sleep 1000)
+      (async/>!! out-ch {:ack "ok"})
+      (Thread/sleep 10)
       (if (> (t/in-millis (t/interval @last-msg-time (l/local-now))) 10000)
         (when-not (s/closed? stream) (s/close! stream))
         (recur))))
